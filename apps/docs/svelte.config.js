efea4695dde82897f999c652545400ddb5e924fd
Marek Jóźwiak <168720167+66HEX@users.noreply.github.com>
2025-12-26 18:14:39 +0100

    chore(docs): install vercel adapter

--- apps/docs/svelte.config.js
+++ apps/docs/svelte.config.js
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from "node:url";
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import { escapeSvelte, mdsvex } from "mdsvex";
 import { createHighlighter } from "shiki";
