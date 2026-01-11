72c9d2d693d543b283a26ff28ff2a0c4bbbf5a8a
Marek Jóźwiak <168720167+66HEX@users.noreply.github.com>
2026-01-11 12:17:59 +0100

    fix(docs): weightwave mobile demo

--- apps/docs/src/routes/docs/weight-wave/WeightWaveDemo.svelte
+++ apps/docs/src/routes/docs/weight-wave/WeightWaveDemo.svelte
@@ -2,6 +2,10 @@
 	import { WeightWave } from "motion-core";
 </script>
 
-<WeightWave class="text-8xl font-display" baseWeight={200} hoverWeight={900}>
+<WeightWave
+	class="text-6xl font-display md:text-8xl"
+	baseWeight={200}
+	hoverWeight={900}
+>
 	Motion Core
 </WeightWave>
