ddb485ca8318652d4d1b4ec809b382e40c0f8605
Marek Jóźwiak <168720167+66HEX@users.noreply.github.com>
2026-03-25 19:44:32 +0100

    fix(motion-core): add gsap dist module declarations

--- packages/motion-core/src/lib/types.d.ts
+++ packages/motion-core/src/lib/types.d.ts
@@ -27,3 +27,21 @@ declare module "*.geojson?raw" {
 	const value: string;
 	export default value;
 }
+
+declare module "gsap/dist/gsap" {
+	export * from "gsap";
+	export { gsap } from "gsap";
+	export { gsap as default } from "gsap";
+}
+
+declare module "gsap/dist/ScrollTrigger" {
+	export * from "gsap/ScrollTrigger";
+	export { ScrollTrigger } from "gsap/ScrollTrigger";
+	export { ScrollTrigger as default } from "gsap/ScrollTrigger";
+}
+
+declare module "gsap/dist/SplitText" {
+	export * from "gsap/SplitText";
+	export { SplitText } from "gsap/SplitText";
+	export { SplitText as default } from "gsap/SplitText";
+}
